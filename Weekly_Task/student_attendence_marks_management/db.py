acee50df101c5de1199fa6c599915a21e21842f0
Mohan <adapamohan74@gmail.com>
2026-03-27 11:29:09 +0530

    phase 1

--- Weekly_Task/student_attendence_marks_management/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Mohan@2406",
-        database="attendance_db"
-    )
